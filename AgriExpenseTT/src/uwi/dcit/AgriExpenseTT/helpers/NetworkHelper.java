4482cbc4be6ff5483b0e784662f5977d9b063a62
Kyle E DeFreitas <kyle.e.defreitas@gmail.com>
2014-11-29 23:31:42 -0400

    created a helper for network tasks

---
+++ AgriExpenseTT/src/uwi/dcit/AgriExpenseTT/helpers/NetworkHelper.java
@@ -0,0 +1,15 @@
+package uwi.dcit.AgriExpenseTT.helpers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class NetworkHelper {
+
+	public static boolean isNetworkAvailable(Activity activity){
+		ConnectivityManager connectivityManager = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
+}
